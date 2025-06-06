aa419c1e1f6da08cfcaaf429295117c1ad832bc3
Brandon <tricksterguy87@gmail.com>
2025-06-06 16:12:17 -0700

    Full clan code

---
+++ FullClan.Plugin/Plugin.cs
@@ -0,0 +1,136 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace FullClan.Plugin
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger = new(MyPluginInfo.PLUGIN_GUID);
+        public void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    // Patch allows you to roll Clan/Clan
+    [HarmonyPatch(typeof(SaveManager), "GetRandomUnlockedClass")]
+    public class SaveManager_GetRandomUnlockedClass_AvoidSameClassPatch
+    {
+        public static void Prefix(ref ClassData? avoidClass)
+        {
+            avoidClass = null;
+        }
+    }
+
+
+    [HarmonyPatch(typeof(RunSetupScreen), "HandleClanOptionsSelected")]
+    public class RunSetupScreen_GetRandomUnlockedClass_DisableSwapPatch
+    {
+        // Gathering private methods to invoke them later
+        // C# specific ? makes the type nullable, means that null could be a value instead of the type present.
+        static readonly MethodInfo? RefreshCharacters = typeof(RunSetupScreen).GetMethod("RefreshCharacters", BindingFlags.NonPublic | BindingFlags.Instance);
+        static readonly MethodInfo? RefreshClanCovenantUI = typeof(RunSetupScreen).GetMethod("RefreshClanCovenantUI", BindingFlags.NonPublic | BindingFlags.Instance);
+        static readonly MethodInfo? RefreshWinStreak = typeof(RunSetupScreen).GetMethod("RefreshWinStreak", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        // Full rewrite patch, these should be avoided in general. Simple enough method that it shouldn't cause too much of an issue.
+        // Access fields in the object you are patching with "___"
+        // Access the object itself with "__instance"
+        public static void Prefix(bool isMainClass, RunSetupClanSelectionLayoutUI.ClanOptionData? newClanOptionData, RunSetupClassLevelInfoUI ___mainClassInfo, RunSetupClassLevelInfoUI ___subClassInfo, SaveManager ___saveManager, RunSetupScreen __instance)
+        {
+            RunSetupClassLevelInfoUI runSetupClassLevelInfoUI = (isMainClass ? ___mainClassInfo : ___subClassInfo);
+            if (newClanOptionData != null && newClanOptionData.IsRandom)
+            {
+                runSetupClassLevelInfoUI.SetClassRandom(newClanOptionData.randomId);
+            }
+            else if (___saveManager != null)
+            {
+                ClassData? classData = newClanOptionData?.clanData;
+                int setLevel = 0;
+                if (classData != null)
+                {
+                    setLevel = ___saveManager.GetClassLevelInMetagame(classData.GetID());
+                    runSetupClassLevelInfoUI.SetClass(classData, setLevel, runSetupClassLevelInfoUI.ChampionIndex);
+                }
+            }
+
+            // C# specific this is guaranteed to not be null so use null forgiveness operator
+            // RefreshCharacters(delaySfx: false);
+            RefreshCharacters!.Invoke(__instance, [false]);
+
+            // RefreshClanCovenantUI();
+            RefreshClanCovenantUI!.Invoke(__instance, []);
+
+            // RefreshWinStreak();
+            RefreshWinStreak!.Invoke(__instance, []);
+
+            ___mainClassInfo.ShowCardPreview();
+            ___subClassInfo.ShowCardPreview();
+        }
+    }
+
+    // Patch ensures you can access the Banner on Ring 2.
+    // Apparently it moves it to the center which was unexpected (not complaining).
+    [HarmonyPatch(typeof(RandomMapDataContainer), nameof(RandomMapDataContainer.GetMapNodeData))]
+    public class RandomMapDataContainer_GetMapNodeData_ForceDoubleBannersOnFullClanPatch
+    {
+        public static void Prefix(RandomMapDataContainer __instance, ref bool ___disallowDuplicatesOnSameSection, SaveManager saveManager)
+        {
+            // Lazy override field if the classes match. You you play with a normal clan combination it should reset back to the default value.
+            if (__instance.name == "RewardsSimpleRing2Banners")
+            {
+                ___disallowDuplicatesOnSameSection = saveManager.GetMainClass() != saveManager.GetSubClass();
+            }
+        }
+    }
+
+    // Patch that prevents a bug with the showcase cards.
+    // If the same card is pulled the UI is thrown off only showing 2 cards.
+    [HarmonyPatch(typeof(CardSetBuilder), nameof(CardSetBuilder.CollectCards))]
+    public class CardSetBuilder_CollectCards_PreventSameCommonStartOfCardsPatch
+    {
+        public static List<CardData> extraPull = [];
+
+        public static void Postfix(CardSetBuilder __instance, ref List<CardData> collectedCards, ref List<CardData> showcaseCards, List<CardPull> ___paramCardPulls, SaveManager saveManager)
+        {
+            if (__instance.name != "StarterDeck_C01" || saveManager.GetMainClass() != saveManager.GetSubClass())
+                return;
+
+            // Reroll and modify main common card if MainClassCommon2X and SubClassCommon2X pulled the same card.
+            if (showcaseCards[0] == showcaseCards[1] && showcaseCards[1] == showcaseCards[2] && showcaseCards[2] == showcaseCards[3])
+            {
+                Plugin.Logger.LogInfo("Rerolling due to same cards.");
+                if (___paramCardPulls[0]?.name != "MainClassCommon2X")
+                {
+                    Plugin.Logger.LogWarning("Possible mod incompatibility, ParamCardPulls has been tampered with! Bail!");
+                    return;
+                }
+
+                int i = 0;
+                do
+                {
+                    extraPull.Clear();
+                    ___paramCardPulls[0].CollectCards(extraPull, saveManager);
+                    i++;
+                    Plugin.Logger.LogInfo("Rerolling again due to same cards.");
+                } while (extraPull[0] == showcaseCards[0] && i < 200);
+
+                // Should be rerolled (1/8)**200 chance of failure.
+                for (i = 0; i < extraPull.Count; i++)
+                {
+                    showcaseCards[i] = extraPull[i];
+                    collectedCards[i] = extraPull[i];
+                }
+            }
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # FullClan
-Remade version of the Full Clan mod from Monster Train 1. Example mod with Harmony Patches to change game behavior
+
+Noncosmetic mod that allows you to play with the same clan as primary and allied, and as an example of a mod using Harmony that changes game code.
+
+
+
+## Features
+
+Allows you to select the same clan as your primary clan, and allows you to select if you want double the starter cards as the champion or you may use the alternate champions starter card.
+
+For ring 2 the banner will be present in the middle allowing you to take it no matter which path you choose since the game does not allow the same banner node on both sides. (Unfortunately it doesn't look as nice as if the banner was placed on both sides)
+
+
+
+## Known Issues
+
+While it appears that the wins are tracked (as indicated in screenshots). The logbook page with all of the champion combinations and wins won't show the same clan combination.
+
+The tracking for win runs with all clan combinations (titan victories) will still show 180 combinations and not 200.
+
+Unit upgrade shop may offer a cheap common unit upgrade (i.e. Heartstone at 15 gold instead of 25 gold). I suppose this was the slot of the other clan specific upgrade, but since it appeared before it got replaced.
+
+
+
+## Warning
+
+As with all non cosmetic mods and as a warning **do not** do anything online such as community challenges with a main/sub clan as the same clan. It should still be safe to do daily challenges with the mod installed (though I'd still recommend to run the game with all mods disabled just in case.)
+
+
+
+### **Important**
+
+F8 bug reports **will be disabled** with this mod enabled (you can still open the menu, but submitting a report won't send it anywhere). This feature was asked for directly from Shiny Shoe and should be present in most mods. If you find a bug in the game and suspect that it isn't due to your use of mods, file a report at [Monster Train 2 Bugs and Technical issues :: Steam Community](https://steamcommunity.com/app/2742830/discussions/1/) or on the Shiny Shoe discord.
+
+
+
+## Lastly
+
+We are not responsible for any unintended behavior / damage that results from using this mod. Use responsibly.
